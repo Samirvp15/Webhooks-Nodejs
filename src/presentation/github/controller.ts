8bbdc78d282f8cc228fbbea0b5a3f99534fa2d63
Samir Vergara Pastor <132174621+Samirvp15@users.noreply.github.com>
2024-10-02 14:58:28 -0500

    add discordService for Discord Webhook

--- src/presentation/github/controller.ts
+++ src/presentation/github/controller.ts
@@ -1,11 +1,13 @@
 import { Request, Response } from "express";
 import { GitHubService } from "../services/github.service";
+import { DiscordService } from "../services/discord.service";
 
 
 export class GithubController {
 
     constructor(
         private readonly githubService = new GitHubService(),
+        private readonly discordService = new DiscordService(),
     ) { }
 
     webhookHandler = (req: Request, res: Response) => {
@@ -29,10 +31,9 @@ export class GithubController {
                 break;
         }
 
-        console.log({ message });
-
-
-        res.status(202).json('xdd');
+      this.discordService.notify(message)
+      .then( () =>  res.status(202).json('Acepted'))
+      .catch((error)=> res.status(500).json(error));
     }
 
 
